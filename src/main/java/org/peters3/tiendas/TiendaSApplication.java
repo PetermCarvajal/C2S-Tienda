8e50d11ff3f64e041ba654a8f7eb1e380a15f03b
PetermCarvajal <pmcarvajal_77@cue.edu.co>
2025-03-18 08:12:11 -0500

    No correl mi Tomcat desde el IDE

---
+++ src/main/java/org/peters3/tiendas/TiendaSApplication.java
@@ -0,0 +1,13 @@
+package org.peters3.tiendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/peters3/tiendas/TiendaSApplicationTests.java
@@ -0,0 +1,13 @@
+package org.peters3.tiendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
