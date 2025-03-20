0039697ed6aeba73026f81bdca0888ce28f0c191
PetermCarvajal <pmcarvajal_77@cue.edu.co>
2025-03-20 10:25:27 -0500

    6

--- src/main/java/org/peters3/tiendas/Authentication/AuthController.java
+++ src/main/java/org/peters3/tiendas/Authentication/AuthController.java
@@ -13,7 +13,7 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
 
     private final AuthService authService;
-    private final UserRepository userRepository; // Repositorio de usuarios
+    private final UserRepository userRepository;
 
     @PostMapping("login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
@@ -30,22 +30,23 @@ public class AuthController {
         return ResponseEntity.ok("Enviando GET ");
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<?> actualizarUsuario(@PathVariable int id, @RequestBody RegisterRequest request) {
-        return userRepository.findById(id).map(usuario -> {
+    @PutMapping("/{username}")
+    public ResponseEntity<?> actualizarUsuario(@PathVariable String username, @RequestBody RegisterRequest request) {
+        return userRepository.findByUsername(username).map(usuario -> {
             usuario.setUsername(request.getUsername());
             usuario.setNombre(request.getNombre());
             usuario.setApellido(request.getApellido());
             usuario.setPassword(request.getPassword());
             usuario.setRole(Role.valueOf(request.getRole()));
+            usuario.setEmail(request.getEmail());
             userRepository.save(usuario);
             return ResponseEntity.ok(usuario);
         }).orElse(ResponseEntity.notFound().build());
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> eliminarUsuario(@PathVariable int id) {
-        return userRepository.findById(id).map(usuario -> {
+    @DeleteMapping("/{username}")
+    public ResponseEntity<?> eliminarUsuario(@PathVariable String username) {
+        return userRepository.findByUsername(username).map(usuario -> {
             userRepository.delete(usuario);
             return ResponseEntity.ok("Usuario eliminado con éxito");
         }).orElse(ResponseEntity.notFound().build());
